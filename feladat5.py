4887d9d40dc601144300ce3edf526ffdfb0f62e6
PajgerZs <pajgerzs@kkszki.hu>
2025-12-04 17:23:15 +0100

    Task 5 is finished.

--- feladat5.py
+++ feladat5.py
@@ -1,3 +1,10 @@
 #5. feladat – Lista elemeinek négyzetre emelés
 #Adott egy lista, készíts új listát, amely minden elem négyzetét tartalmazza.
 
+szamok = [2,3,4,5,6,7,8,9,10]
+negyzetek = []
+
+for i in range(len(szamok)):
+    negyzetek.append(szamok[i]**2)
+
+print(f"A számok négyzetei: {negyzetek}")
