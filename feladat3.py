0551190f3c6fc0d11f41af4059e08860011e6a79
PajgerZs <pajgerzs@kkszki.hu>
2025-12-04 16:52:56 +0100

    Task 3 is finished.

---
+++ feladat3.py
@@ -0,0 +1,18 @@
+#3. feladat – Prímszám ellenőrzése
+#Döntsd el egy számról, hogy prímszám-e (szelekció + iteráció).
+
+n = int(input("Kérem az egész számot: "))
+prim = True
+
+if n < 2:
+    prim = False
+else:
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            prim =False
+            break
+
+if prim == True:
+    print("A megadott szám prímszám.")
+else:
+    print("A megadott szám NEM prímszám.")
